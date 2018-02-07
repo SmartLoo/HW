98d273fa2a080bdfce8dccfe10c31562d306d130
Benjamin Corn <bcorn@bu.edu>
2018-02-07 15:14:11 -0500

    Updating POST request given updated API format.

--- ESP8266toAPI.ino
+++ ESP8266toAPI.ino
@@ -16,8 +16,6 @@ int code = 0;
 
 void setup()
 {
-  //Serial.begin(111111); 
-  //Serial.begin(115200); //for ESP  
   Serial.begin(9600);
   delay(100);
 
@@ -39,11 +37,6 @@ void setup()
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  //Serial.print("Netmask: ");
-  //Serial.println(WiFi.subnetMask());
-  //Serial.print("Gateway: ");
-  //Serial.println(WiFi.gatewayIP());
-
 }
 
 
@@ -63,34 +56,7 @@ int GetData()
   }
 }
 
-void GetSensorLevel()
-{
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-  }
-  // We now create a URL for the request
-  String url = "/api/sensor";
-
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-  delay(500);
-
-  // Read all the lines of the reply from server and print them to Serial
-  if (client.available())
-  {
-    String line = client.readStringUntil('"');
-    data = client.readStringUntil('"').toInt();
-    Serial.write((unsigned char)data);
-    Serial.flush();
-    Serial.println((unsigned char)data);
-  }
-}
-
-void PostSensorLevel(int t)
+void PostSensorLevel(String sensorId, int sVal, int bLevel)
 {
   WiFiClient client;
   const int httpPort = 80;
@@ -101,12 +67,13 @@ void PostSensorLevel(int t)
   
   // We now create a URL for the request
   String url = "/api/sensor_data/update";
-  String val = String(t, DEC);
-  String req = "{\"building\": \"PHO\",\"location\": \"1FLOORW\",\"sensorId\": \"302\",\"value\":" + val + ",\"battery\": 10}";
+  String sensorValue = String(sVal, DEC);
+  String batteryLevel = String(bLevel, DEC);
+  String req = "{\"sensorId\": \"" + sensorId + "\",\"sensorValue\": \"" + sensorValue + "\",\"batteryLevel\": \"" + batteryLevel + "\"}";
  
 
   HTTPClient http;
-  http.begin("http://smartloo.azurewebsites.net/api/sensor_data/update");
+  http.begin("http://smartloo.azurewebsites.net/api/sensor");
   http.addHeader("Content-Type", "application/json");
 
   int httpCode = http.POST(req);
@@ -116,18 +83,6 @@ void PostSensorLevel(int t)
   Serial.println(httpCode);
   Serial.println(payload);
   http.end();
-
-/*
-  // This will send the request to the server
-  client.print("POST /api/sensor_data/update HTTP/1.1");
-  client.print("Host: smartloo.azurewebsites.net");
-  client.println("Content-Type: application/json"); 
-  client.println("Accept: application/json");
-  client.print("Content-Length: ");
-  client.println(req.length());
-  client.println();
-  client.println(req);
-*/
 
   Serial.println("POST REQUEST MADE.");  
 }
