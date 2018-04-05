465fcb98f9164cf5050825d9c169ee01a5796428
Benjamin Corn <bcorn@bu.edu>
2018-04-05 16:24:52 -0400

    Updated US

--- USReader.ino.ino
+++ USReader.ino.ino
@@ -1,43 +1,72 @@
-//define pins numbers
-const int trigPin = 7;
-const int echoPin = 8; 
-// defines variables
-long duration;
-int distance; 
+/**
+ * Hardware Connections:
+ *  Arduino | HC-SR04 
+ *  -------------------
+ *    5V    |   VCC     
+ *    7     |   Trig     
+ *    8     |   Echo     
+ *    GND   |   GND
+ *  
+ */
+
+// Pins
+const int TRIG_PIN = 7;
+const int ECHO_PIN = 8;
+const String DEVICE_GUID = "Pdac15423-20d7-4e97-80cd-0e46c98cd80e";
 String stringDistance;
-String guid = "Rdac15423-20d7-4e97-80cd-0e46c98cd60e:";
-String message;
 String batteryLevel;
+String message;
+
+// Anything over 400 cm (23200 us pulse) is "out of range"
+const unsigned int MAX_DIST = 23200;
 
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT); 
-  pinMode(13, OUTPUT);
+
+  // The Trigger pin will tell the sensor to range find
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // We'll use the serial monitor to view the sensor output
   Serial.begin(9600);
 }
 
 void loop() {
 
-  digitalWrite(trigPin, LOW); // Clears the trigPin
-  delayMicroseconds(2);
-  
-  digitalWrite(trigPin, HIGH); // Sets the trigPin on HIGH state for 10 micro seconds
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
-  
-  distance= duration*0.034/2; // Calculating the distance
-  stringDistance = String(distance);
-  batteryLevel = ":75";
-  message = guid + stringDistance + batteryLevel;
-  digitalWrite(13, HIGH);
-  Serial.println(message); // Prints the distance on the Serial Monitor
-  //52858b18-0bb7-45b0-836c-d4d5f335558e
-  //dac15423-20d7-4e97-80cd-0e46c98cd60e
-  //Serial.write(distance);
-  delay(5000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+  float inches;
 
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Wait for pulse on echo pin
+  while ( digitalRead(ECHO_PIN) == 0 );
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+  // Calculate distance in centimeters and inches. The constants
+  // are found in the datasheet, and calculated from the assumed speed 
+  //of sound in air at sea level (~340 m/s).
+  cm = pulse_width / 58.0;
+
+  stringDistance = String((int)cm);
+  batteryLevel = ":75";
+  message = DEVICE_GUID + stringDistance + batteryLevel;
+
+  if (cm < 200)
+  {
+      Serial.println(message); // Prints the distance on the Serial Monitor
+  }
+  
+  // Wait 6s before next measurement
+  delay(6000);
 }
