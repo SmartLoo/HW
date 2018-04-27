d61666069a9bec1b7873f98e431aba2a34bafa98
bng8 <ngb@bu.edu>
2018-04-27 00:58:36 -0400

    code cleanup for water sensor code and introduced WDT optimization to water sensor code

--- groveSender/groveSender.ino
+++ groveSender/groveSender.ino
@@ -1,10 +1,9 @@
 /**
  * Hardware Connections:
- *  Arduino | HC-SR04 
+ *  Arduino | Grove Water Sensor 
  *  -------------------
- *    5V    |   VCC     
- *    7     |   Trig     
- *    8     |   Echo     
+ *    5V    |   VCC      
+ *    8     |   SIGNAL     
  *    GND   |   GND
  *  
  */
@@ -16,85 +15,45 @@ const int minSleepCycles = (minutesToSleep*60)/8;
 
 const int enableXBee = 4;
 const int enable5V = 6;
-const int TRIG_PIN = 7;
-const int ECHO_PIN = 8;
+const int WATER_SENSOR_SIGNAL_LINE = 8;
 
-const String DEVICE_GUID = "Pdac15423-20d7-4e97-80cd-0e46c98cd80e";
-String stringDistance;
+const String DEVICE_GUID = "W52858b18-0bb7-45b0-836c-d4d5f335558e";
 String message;
 
-// Anything over 400 cm (23200 us pulse) is "out of range"
-const unsigned int MAX_DIST = 23200;
-
-void setup() {
+void setup()
+{
   for(int i = 0; i <= 20; i++){
     if(i != 8){
       pinMode(i, OUTPUT);
       digitalWrite(i, LOW);
     }
   }
-
+  pinMode(WATER_SENSOR_SIGNAL_LINE, INPUT);
   watchdogOn(); // Turn on the watch dog timer.
-
-  // Disable the ADC by setting the ADEN bit (bit 7) to zero.
-  //ADCSRA = ADCSRA & B01111111;
-
-  // Disable the analog comparator by setting the ACD bit
-  // (bit 7) to one.
-  //ACSR = B10000000;
-
-  // Disable digital input buffers on all analog input pins
-  // by setting bits 0-5 to one.
-  //DIDR0 = DIDR0 | B00111111;
-  
   Serial.begin(9600);
 }
 
-void loop() {
-
-  unsigned long t1;
-  unsigned long t2;
-  unsigned long pulse_width;
-  float cm;
-  float inches;
-
-  enterSleep(); // ATmega328 goes to sleep for about 8 seconds
-  // and continues to execute code when it wakes up
+void loop()
+{
+  enterSleep();
 
   if (sleepCycleCounter > minSleepCycles) {
     digitalWrite(enable5V, HIGH);
     delay(100);
-    // Hold the trigger pin high for at least 10 us
-    digitalWrite(TRIG_PIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG_PIN, LOW);
-
-    // Wait for pulse on echo pin
-    while ( digitalRead(ECHO_PIN) == 0 );
-
-    // Measure how long the echo pin was held high (pulse width)
-    // Note: the micros() counter will overflow after ~70 min
-    t1 = micros();
-    while ( digitalRead(ECHO_PIN) == 1);
-    t2 = micros();
-    pulse_width = t2 - t1;
-
-    cm = pulse_width / 58.0;
-
-    stringDistance = String((int)cm);
-    message = DEVICE_GUID + ":" + stringDistance;
-
+    if(digitalRead(WATER_SENSOR_SIGNAL_LINE) == LOW){
+      message = DEVICE_GUID + ":1";
+    }
+    else{
+      message = DEVICE_GUID + ":0";
+    }
     digitalWrite(enableXBee, HIGH);
     delay(500);
-    if (cm < 200)
-    {
-        Serial.println(message);
-    }
+    Serial.println(message);
     delay(500);
     digitalWrite(enable5V, LOW);
     digitalWrite(enableXBee, LOW);
     sleepCycleCounter = 0;
-  }
+  } 
 }
 
 void enterSleep()   
